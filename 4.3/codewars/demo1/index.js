cf64345b2e3de091a7a60061ac07a767ebb6991b
weicheng1997 <48019425+weicheng1997@users.noreply.github.com>
2019-04-08 16:08:38 +0800

    4.3

---
+++ 4.3/codewars/demo1/index.js
@@ -0,0 +1,14 @@
+function generateHashtag(str){
+    return str.length > 140 || str === ''? false:'#' +str.split(' ').map(capitalize).join(' ')
+// if (str.length > 140||str ==='')
+// return false;
+
+// str =str.split(' ').map(word => word.charAt(0).toUpperCase()+word.slice(1)).join(' ')
+// str='#'+str;
+// return str;
+// str.split(' ').map(word => word.charAt(0).toUpperCase()+word.slice(1)).join(' ')
+}
+function capitalize(word){
+    return word.charAt(0).toUpperCase()+word.slice(1);
+}
+console.log(generateHashtag('hello miss dong'))
